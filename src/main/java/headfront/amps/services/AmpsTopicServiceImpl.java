912efc6446600496e4a8b1a7cb3cd3b0573ee9ff
Deepak D <deepakcdo@gmail.com>
2021-04-27 12:43:24 +0100

    Exclude JetfuelExectute topics with CLIENT_J in them

--- src/main/java/headfront/amps/services/AmpsTopicServiceImpl.java
+++ src/main/java/headfront/amps/services/AmpsTopicServiceImpl.java
@@ -127,8 +127,8 @@ public class AmpsTopicServiceImpl implements TopicService {
                                             String strTopic = topic.toString();
                                             String strFitler = filter.toString();
                                             if (strFitler.contains(JetFuelExecuteConstants.FUNCTION_PUBLISHER_NAME)) {
-                                                // Dont choose topics with CLIENT
-                                                if (strTopic != null && !strFitler.contains("CLIENT")) {
+                                                // Dont choose topics with CLIENT_J
+                                                if (strTopic != null && !strFitler.contains("CLIENT_J")) {
                                                     jetFuelExecuteFunction = strTopic;
                                                 }
                                             }
@@ -144,8 +144,8 @@ public class AmpsTopicServiceImpl implements TopicService {
                                             String strTopic = topic.toString();
                                             String strFitler = filter.toString();
                                             if (strFitler.contains(JetFuelExecuteConstants.FUNCTION_CALLER_HOSTNAME)) {
-                                                // Dont choose topics with CLIENT
-                                                if (strTopic != null && !strFitler.contains("CLIENT")) {
+                                                // Dont choose topics with CLIENT_J
+                                                if (strTopic != null && !strFitler.contains("CLIENT_J")) {
                                                     jetFuelExecuteFunctionBus = strTopic;
                                                 }
                                             }
